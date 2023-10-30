c3ec3df2035c96de8d80ab0cde25d1a0aa046078
Artur Hamernik <hamernik18@gmail.com>
2023-10-30 23:35:01 +0100

    validations

--- contracts/CertificateRepository.sol
+++ contracts/CertificateRepository.sol
@@ -2,17 +2,19 @@
 pragma solidity >=0.4.22 <0.9.0;
  
 contract CertificateRegistry {
-
+    
     address contractOwner;
 
-    struct Recipient{ // recipier description
+    struct Recipient { // recipient description
+        // Should recipient have some kind of ID? Or should recipient have address?
         string name;
         string surname;
     }
  
-     struct Certificate {
-        string checksum; // path to certificate
-        uint256 timestamp;
+    struct Certificate {
+        string checksum; // unique hash of certificate generated on frontend
+        uint256 issueDate;
+        uint256 expireDate;
         address issuer;
         Recipient recipient;
     }
@@ -23,21 +25,25 @@ contract CertificateRegistry {
 
     constructor() {
         contractOwner = msg.sender;
-        trustedIssuers[msg.sender] = true; // first admin address being used to add trusted issuers
+        trustedIssuers[msg.sender] = true; // contract owner address as first trusted issuer
     }
  
     modifier onlyTrustedIssuer() {
-        require(trustedIssuers[msg.sender], "You are not a trusted issuer.");
+        require(trustedIssuers[msg.sender], "You are not a trusted issuer!");
         _;
     }
  
-    //TODO 1: Walidatory (puste pola itd)
-    function addCertificate(string memory _recipient_name, string memory _recipient_surname, string memory _checksum, address _issuer) public onlyTrustedIssuer{
-        certificates[_checksum] = Certificate(_checksum, block.timestamp, _issuer, Recipient(_recipient_name, _recipient_surname));
+    function addCertificate(string memory _checksum, string memory _recipient_name, string memory _recipient_surname) public onlyTrustedIssuer {
+        require(keccak256(bytes(certificates[_checksum].checksum)) != keccak256(bytes(_checksum)), "Certificate already present!");
+        require(bytes(_checksum).length > 0, "File checksum must not be empty!");
+        require(bytes(_recipient_name).length > 0, "Recipent name must not be empty!");
+        require(bytes(_recipient_surname).length > 0, "Recipent surname must not be empty!");
+
+        certificates[_checksum] = Certificate(_checksum, block.timestamp, block.timestamp + 30 days, msg.sender, Recipient(_recipient_name, _recipient_surname));
     }
 
-    function addTrustedIssuer(address _newIssuer) public onlyTrustedIssuer {
-        trustedIssuers[_newIssuer] = true;
+    function addTrustedIssuer(address _issuer) public onlyTrustedIssuer {
+        trustedIssuers[_issuer] = true;
     }
 
     function removeTrustedIssuer(address _issuer) public onlyTrustedIssuer {
@@ -46,9 +52,8 @@ contract CertificateRegistry {
         }
     }
 
-    //TODO 2: getCertificate
-
-    function getCertificate (string memory _checksum) public view onlyTrustedIssuer returns (Certificate memory result) {
+    // Anyone should be able to download certificate, unless recipients also has to be trusted
+    function getCertificate(string memory _checksum) public view returns (Certificate memory result) {
         result = certificates[_checksum];
     }
 }
